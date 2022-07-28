55d34d88e024050356d8661a5c866e9d361bff37
Jobhandsome <1145938682@qq.com>
2022-07-28 20:58:00 +0800

    Revert "调整 api.category.get-lists 支持pid子节点查询，支持分类名称模糊查询"
    This reverts commit 25e7757fe3fda69efecdf9dbe4c79f7695089f66.

--- api/v1/controller/CategoryController.go
+++ api/v1/controller/CategoryController.go
@@ -9,7 +9,6 @@ import (
 
 type ParamsRequest struct {
 	Name string `form:"name" json:"name"`
-	Pid  int64  `form:"pid" json:"pid"` // pid 上级节点ID
 }
 
 // GetCategoryLists 获取分类列表
@@ -21,20 +20,16 @@ func GetCategoryLists(ctx *gin.Context) {
 	}
 	// 过滤page和pageSize
 	paramsMap, _ := utils.AnyToMap(params)
-	ParamsFilter := utils.ParamsFilter("page,pageSize,name", paramsMap)
+	ParamsFilter := utils.ParamsFilter("page,pageSize", paramsMap)
 	// 获取列表
 	DB := config.GetDB()
 	var Result []*model.Category
-	var resErr error
-	// 如果name条件不为空，追加模糊查询：position('搜索字符' in 字段)
-	if len(params.Name) > 0 {
-		resErr = DB.Where(ParamsFilter).Where("position(? in name)", params.Name).Find(&Result).Error
-	} else {
-		resErr = DB.Where(ParamsFilter).Find(&Result).Error
-	}
+	resErr := DB.Where(ParamsFilter).Find(&Result).Error
 	if resErr != nil {
 		utils.Fail(ctx, resErr.Error(), nil)
 		return
 	}
-	utils.Success(ctx, "获取成功", Result)
+	var category model.Category
+	ResultLists := category.ToTree(Result)
+	utils.Success(ctx, "获取成功", ResultLists)
 }
