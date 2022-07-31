12110107456e6153a5935372dcff9ff0552cf6dc
Jobhandsome <1145938682@qq.com>
2022-07-31 21:17:01 +0800

    api.goods.detail 商品详情接口，链表查询，属性切割处理

--- api/v1/controller/GoodsController.go
+++ api/v1/controller/GoodsController.go
@@ -5,6 +5,7 @@ import (
 	"goshop/config"
 	"goshop/model"
 	"goshop/utils"
+	"strings"
 )
 
 type GoodsDetailsParamsRequest struct {
@@ -13,7 +14,8 @@ type GoodsDetailsParamsRequest struct {
 
 type GoodsDetailsResult struct {
 	model.Goods
-	CateName string `json:"cate_name"` // 分类名称
+	CateName          string   `json:"cate_name"`         // 分类名称
+	GoodsPropertyList []string `json:"goodsPropertyList"` // 商品属性列表
 }
 
 // GoodsDetails 商品详情
@@ -25,12 +27,14 @@ func GoodsDetails(ctx *gin.Context) {
 	}
 	var GoodDetail GoodsDetailsResult
 	DB := config.GetDB()
-	//resErr := DB.Where("id = ?", params.ID).Find(&GoodDetail).Error
 	resErr := DB.Model(&model.Goods{}).Select("goods.*, categories.name as cate_name").Joins("left join categories on categories.id = goods.goods_cate").Where("goods.id = ?", params.ID).Scan(&GoodDetail).Error
 	if resErr != nil {
 		utils.Fail(ctx, resErr.Error(), nil)
 		return
 	}
 
+	GoodDetail.GoodsPropertyList = strings.Split(GoodDetail.GoodsProperty, ";")
+
 	utils.Success(ctx, "获取成功", GoodDetail)
+	return
 }
